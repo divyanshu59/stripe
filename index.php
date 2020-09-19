7b0cdcf3f8873a3eafc72653a7d2a0d427904d2f
KartikPatelOfficial <patelkartik1910@gmail.com>
2020-09-19 13:52:14 +0530

    Remove: PaymentIntent

--- index.php
+++ index.php
@@ -7,15 +7,6 @@ $email = $_POST['email'];
 $stripe = new \Stripe\StripeClient(
     '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'
 );
-$stripe->paymentIntents->create([
-    'amount' => $amount,
-    'currency' => 'usd',
-    'payment_method_types' => ['card'],
-    'payment_method' => $payment_method_id,
-    'stripe_account' => 'acct_1HQvaqJ5lzJBB3vD',
-    'receipt_email' => $email,
-    'confirm' => true
-]);
 
 $charge = $stripe->charges->create([
     'amount' => $amount,
