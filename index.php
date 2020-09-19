73dc0bc3f32c31857eec9dd717670b040bdb5b89
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-19 15:52:06 +0530

    Fix: Index Php

--- index.php
+++ index.php
@@ -4,18 +4,19 @@ $amount = $_GET['amount'];
 $payment_method_id = $_GET['payment_method_id'];
 $email = $_GET['email'];
 
+
 $stripe = new \Stripe\StripeClient(
     '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'
 );
-$stripe->paymentIntents->create([
+$paymentIntent = $stripe->paymentIntents->create([
     'amount' => $amount,
     'currency' => 'usd',
     'payment_method_types' => ['card'],
     'payment_method' => $payment_method_id,
-    'stripe_account' => 'acct_1HQvaqJ5lzJBB3vD',
     'receipt_email' => $email,
     'confirm' => true
 ]);
-echo $customer;
+
+echo json_encode($paymentIntent);
 
 //header('Location:' . $customer['charges']['url']);
