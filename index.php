0e9745389a730f0622e36c1f46aa49fbc04f4b65
divyanshu59 <greatdivyanshu59@gmail.com>
2020-09-19 13:40:49 +0530

    Add: New Stripe System

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 require_once('vendor/autoload.php');
-
-
+$amount = $_GET['amount'];
+$payment_method_id = $_GET['payment_method_id'];
+$email = $_GET['email'];
 
 $stripe = new \Stripe\StripeClient(
-    'sk_test_qZpYd0NW2ekyE6W9PtKVrYDB00es75TvLu'
+    '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'
 );
-$customer = $stripe->paymentIntents->create([
-    'amount' => 2000,
+$stripe->paymentIntents->create([
+    'amount' => $amount,
     'currency' => 'usd',
     'payment_method_types' => ['card'],
+    'payment_method' => $payment_method_id,
+    'stripe_account' => 'acct_1HQvaqJ5lzJBB3vD',
+    'receipt_email' => $email,
+    'confirm' => true
 ]);
 echo $customer;
 
